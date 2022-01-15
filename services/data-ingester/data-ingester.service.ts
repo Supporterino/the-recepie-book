7dd8ada9edc718d2fb82f02bc96a86460e8ee66a
Supporterino <lars@roth-kl.de>
2022-01-15 23:10:43 +0100

    Initial commit

---
+++ services/data-ingester/data-ingester.service.ts
@@ -0,0 +1,37 @@
+"use strict";
+
+import {Service, ServiceBroker, Context} from "moleculer";
+
+export default class DataIngesterService extends Service {
+
+	public constructor(public broker: ServiceBroker) {
+		super(broker);
+		this.parseServiceSchema({
+			name: "data-ingester",
+			actions:{
+				/**
+				 * Add a new recepie to the storage
+				 *
+				 */
+				new: {
+                    params: {
+                        name: "string",
+                        description: "string"
+                    },
+					rest: {
+						method: "POST",
+						path: "/new",
+					},
+					async handler(ctx): Promise<string> {
+						return this.ActionHello();
+					},
+				},
+			},
+		});
+	}
+
+	// Action
+	public ActionHello(): string {
+		return "Hello Moleculer";
+	}
+}
