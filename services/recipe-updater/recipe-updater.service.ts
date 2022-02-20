7e6456dd791677622b091a86c657855e6fa5ec65
Supporterino <lars@roth-kl.de>
2022-02-20 17:46:34 +0000

    Switch to submodule typedef and implement recipe updating

---
+++ services/recipe-updater/recipe-updater.service.ts
@@ -0,0 +1,41 @@
+"use strict";
+
+import { Service, ServiceBroker} from "moleculer";
+import { Recipe } from "../../types/recipe";
+import { Tag } from "../../types/tag";
+
+export default class RecipeUpdaterService extends Service {
+	public constructor(public broker: ServiceBroker) {
+		super(broker);
+		this.parseServiceSchema({
+			name: "recipe-updater",
+            version: 1,
+			actions:{
+				updateRecipe: {
+					rest: {
+						path: "/updateRecipe",
+						method: "PATCH",
+					},
+					params: {
+						id: "string",
+						name: {type: "string", optional: true},
+						description: {type: "string", optional: true},
+						ingredients: {type: "array", items: {type: "object", strict: true, props: {name: "string", amount: "number", unit: "string"}}, optional: true},
+						steps: {type: "array", items: "string", optional: true},
+						rating: {type: "number", optional: true},
+						tags: {type: "array", items: "string", optional: true},
+						owner: {type: "string", optional: true},
+					},
+					async handler(ctx): Promise<string> {
+						return await this.updateRecipe(ctx.params);
+					},
+				},
+			},
+		});
+	}
+
+	public async updateRecipe(updatedRecipe: any) {
+		updatedRecipe.updateTimestamp = new Date();
+		return await this.broker.call("v1.data-store.update", updatedRecipe);
+	}
+}
