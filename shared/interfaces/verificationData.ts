d55c536e22cc3b025553683c07859c1934fb3170
Supporterino <lars@roth-kl.de>
2022-05-19 22:38:24 +0200

    adjust types object

--- shared/interfaces/verificationData.ts
+++ shared/interfaces/verificationData.ts
@@ -2,9 +2,7 @@ export interface VerificationData {
 	id: string;
 	verified: boolean;
 	verificationStarted: Date;
-	verificationSuccess: Date;
 	passwordResetStarted: Date;
-	passwordResetInProgress: boolean;
 	verificationToken: string;
 	passwordResetToken: string;
 }
